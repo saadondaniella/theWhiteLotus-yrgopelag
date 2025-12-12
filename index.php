7fd852a53ac6e39196e99c714d932b3a68b21a9e
Daniella Saadon <saadondaniella@gmail.com>
2025-12-12 22:49:14 +0100

    Initial project setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/src/header.php';
+?>
+
+<main>
+    <p>Welcome to The White Lotus.</p>
+</main>
+
+</body>
+
+</html>
---
+++ src/header.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>The White Lotus</title>
+    <link rel="stylesheet" href="/public/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>The White Lotus</h1>
+        <p>Cozea Island</p>
+    </header>
